9a3bac98059b78c510a37e58e28b0eb4032fdbeb
Balamoni Chandrasheker <balamoni.c@htcindia.local>
2018-06-18 16:58:49 +0530

    java class changed

--- src/main/java/com/htcindia/SpringbootCrudDemoApplication.java
+++ src/main/java/com/htcindia/SpringbootCrudDemoApplication.java
@@ -2,9 +2,17 @@ package com.htcindia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class SpringbootCrudDemoApplication {
+public class SpringbootCrudDemoApplication  extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootCrudDemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudDemoApplication.class, args);
